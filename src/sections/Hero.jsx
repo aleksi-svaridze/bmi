81525d209f8761f6b6964bcfe62613847901fdb5
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-11-11 13:30:24 +0400

    Add hero card

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,15 @@
 export const Hero = () => {
   return (
     <div className="w-full h-[640px] lg:w-[978px] lg:h-[737px] rounded-b-[35px] bg-gradient-to-br from-[#D6FCFE]/0 to-[#D6E6FE]/100">
-      <h1>Body Mass Index Calculator</h1>
+      <h1 className="font-semibold text-5xl lg:text-[64px] leading-[110%] text-gunmetal">
+        Body Mass Index Calculator
+      </h1>
+      <p>
+        Better understand your weight in relation to your height using our body
+        mass index (BM) calculator. While BMI is not the sole determinant of a
+        healthy weight, it offers a valuable starting point to evaluate your
+        overall health and well-being.
+      </p>
     </div>
   );
 };
