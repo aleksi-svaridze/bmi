589a824f326f8607459b9d07a703e437b4b21b85
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-11-27 22:11:19 +0400

    Start calculator component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,28 @@
 const Calculator = () => {
   return (
-    <div className="bg-green-500 w-full lg:w-[564px] shrink-0">content</div>
+    <div
+      className="bg-white w-full lg:w-[564px] shrink-0 rounded-2xl p-8"
+      style={{ boxShadow: "16px 32px 56px 0 rgba( 143, 174, 207, 0.25)" }}
+    >
+      <h4 className="text-gunmetal text-2xl font-semibold leading-[110%] mb-8">
+        Enter your details below
+      </h4>
+      <div className="flex items-center">
+        <label className="flex items-center gap-x-4 w-1/2 cursor-pointer">
+          <input type="radio" name="bmi" value="metric" className="size-8" />
+          <span className="text-gunmetal text-base font-semibold capitalize leading-[150%]">
+            metric
+          </span>
+        </label>
+
+        <label className="flex items-center gap-x-4 w-1/2 cursor-pointer">
+          <input type="radio" name="bmi" value="imperial" className="size-8" />
+          <span className="text-gunmetal text-base font-semibold capitalize leading-[150%]">
+            imperial
+          </span>
+        </label>
+      </div>
+    </div>
   );
 };
 
--- src/components/LimitationCard.jsx
+++ src/components/LimitationCard.jsx
@@ -1,8 +1,8 @@
 const LimitationCard = ({ title, Icon, body }) => {
   return (
     <div
-      className="w-full lg:w-[365px] md:h-[232px] lg:h-auto rounded-2xl p-8"
-      style={{ boxShadow: "16px 32px 56px 0 rgba( 143, 174, 207, .25)" }}
+      className="w-full lg:w-[365px] md:h-[232px] lg:h-auto rounded-2xl p-8 bg-white"
+      style={{ boxShadow: "16px 32px 56px 0 rgba( 143, 174, 207, 0.25)" }}
     >
       <h3 className="text-gunmetal text-xl flex gap-x-4 items-center mb-4 capitalize font-semibold leading-[110%]">
         <Icon className="size-8" /> {title}
